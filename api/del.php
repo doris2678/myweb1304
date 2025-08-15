3a924af23883d6a98b8005d6f3eda8db9b26b5bc
doris <dorishsh2678@gmail.com>
2025-08-15 15:47:50 +0800

    帳號登入

---
+++ api/del.php
@@ -0,0 +1,7 @@
+<?php include_once "db.php";
+
+$table=$_POST['table'];
+$$table->del($_POST['id']);
+
+
+?>
--- back/admin.php
+++ back/admin.php
@@ -15,10 +15,10 @@
         <td><?=str_repeat("*",mb_strlen($admin['pw']));?></td>            
         <td>
           <?php if($admin['acc']!='admin'):?>  
-            <button data-id="<?=$admin['id'];?>" onclick="location.href='?do=edit_admin&id=<?=$admin['id']?>'">修改</button> 
-            <button data-id="<?=$admin['id'];?>">刪除</button> 
+            <button onclick="location.href='?do=edit_admin&id=<?=$admin['id']?>'">修改</button> 
+            <button class='del-btn' data-id="<?=$admin['id'];?>">刪除</button> 
           <?php else:?>
-            此權限為最高權限  
+            此帳號為最高權限  
           <?php endif;?>  
         </td>
     </tr>
@@ -27,4 +27,19 @@
 <div class="ct">
   <button onclick="location.href='index.php'">返回</button>
 </div>
+
+<script>
+ $('.del-btn').on('click',function(){
+   let id=$(this).data('id');
+   if(confirm('確定要刪除要刪除這筆管理員資料嗎?')){
+    $.post("./api/del.php",{id,table:'Admin'},()=>{
+      location.reload();
+    });
+
+   }
+
+
+ })
+
+</script>
 
